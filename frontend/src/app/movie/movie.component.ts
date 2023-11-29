1076f4b45311f6633ffa0188b9847e4ec8a758f9
Leonardo Coaquira <coaquiraleonardo19@gmail.com>
2023-11-28 23:26:45 -0500

    labfinal

--- frontend/src/app/movie/movie.component.ts
+++ frontend/src/app/movie/movie.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MovieService } from '../movie.service';
+import { NgForm } from '@angular/forms';
+import { Movie } from './movie.model';
 
 @Component({
   selector: 'app-movie',
@@ -7,10 +9,23 @@ import { MovieService } from '../movie.service';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
-  movies: any[] = [];
   currentMovie: any = {};
+  searchTerm = '';
+  movies: Movie[] = [];
+  newImage: any;
+
+  isOperationSuccess = false;
+  isOperationError = false;
+
+  @ViewChild('formName') formName!: NgForm;
+  isFormSubmitted = false;
 
   constructor(private movieService: MovieService) { }
+
+  handleNewImageChange(event: any): void {
+    const file = event.target.files[0];
+    this.newImage = file;
+  }
 
   ngOnInit(): void {
     this.getMovies();
@@ -26,35 +41,33 @@ export class MovieComponent implements OnInit {
   getMovieById(id: string): void {
     this.movieService.getMovieById(id)
       .subscribe((movie) => {
-        this.currentMovie = movie;
+        this.currentMovie = { ...movie }; // Usar spread operator para crear una copia
       });
   }
 
   createMovie(movie: any): void {
-    // Aquí debes agregar lógica para manejar el archivo de imagen.
-    // Puedes utilizar el FormData para enviar archivos.
-
     const formData = new FormData();
     formData.append('title', movie.title);
     formData.append('description', movie.description);
     formData.append('director', movie.director);
     formData.append('genre', movie.genre);
     formData.append('releaseDate', movie.releaseDate);
-    formData.append('image', movie.image);  // 'image' debe coincidir con el nombre en el backend
+    formData.append('image', movie.image);
 
     this.movieService.createMovie(formData)
-      .subscribe(() => {
-        this.getMovies();
-        this.currentMovie = {};
-      });
-  }
-
-  updateMovie(id: string, movie: any): void {
-    this.movieService.updateMovie(id, movie)
-      .subscribe(() => {
-        this.getMovies();
-        this.currentMovie = {};
-      });
+      .subscribe(
+        () => {
+          this.getMovies();
+          this.currentMovie = {};
+          this.isOperationSuccess = true;
+          setTimeout(() => this.isOperationSuccess = false, 5000);
+        },
+        (error) => {
+          console.error('Error creating movie:', error);
+          this.isOperationError = true;
+          setTimeout(() => this.isOperationError = false, 5000);
+        }
+      );
   }
 
   deleteMovie(id: string): void {
@@ -64,12 +77,43 @@ export class MovieComponent implements OnInit {
       });
   }
 
-  editMovie(id: string): void {
-    this.getMovieById(id);
-  }
-
   handleImageChange(event: any): void {
     const file = event.target.files[0];
     this.currentMovie.image = file;
+  }
+
+  onSubmit() {
+    if (this.formName.valid && !this.isFormSubmitted) {
+      this.isFormSubmitted = true;
+      if (this.currentMovie._id) {
+        // Lógica para actualizar si es necesario
+        // Puedes agregar esto si lo necesitas en el futuro
+      } else {
+        // Solo crear, ya que no hay edición
+        this.createMovie(this.currentMovie);
+        this.formName.resetForm();
+        this.isFormSubmitted = false;
+      }
+    }
+  }
+
+  resetForm() {
+    this.formName.resetForm();
+    this.formName.control.markAsUntouched({ onlySelf: true });
+    this.formName.control.markAsPristine({ onlySelf: true });
+  }
+
+  confirmDeleteMovie(movieId: string): void {
+    if (confirm("¿Estás seguro de que quieres eliminar esta película?")) {
+      this.deleteMovie(movieId);
+    }
+  }
+
+  get filteredMovies(): Movie[] {
+    return this.movies.filter(movie =>
+      movie.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      movie.director.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      movie.genre.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
---
+++ frontend/src/app/movie/movie.model.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+    _id: string;
+    title: string;
+    director: string;
+    genre: string;
+    releaseDate: Date;
+    imageUrl: string;
+    // Otras propiedades necesarias
+}
